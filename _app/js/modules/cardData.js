656bd34c4614af1dfe35139ee33049ebf50eded8
Anna Eikrem <eikrem.anna@gmail.com>
2023-03-22 20:55:10 +0100

    Fixed array data

--- _app/js/modules/cardData.js
+++ _app/js/modules/cardData.js
@@ -1,50 +1,52 @@
 export const cardArray = [
 	{
 		name: 'turtle',
-		img: '/_app/assets/sprites/turtle.png',
+		image: '/_app/assets/sprites/turtle.png',
 	},
 	{
 		name: 'cat',
-		img: '/_app/assets/sprites/cat.png',
+		image: '/_app/assets/sprites/cat.png',
 	},
 	{
 		name: 'ghost',
-		img: '/_app/assets/sprites/ghost.png',
+		image: '/_app/assets/sprites/ghost.png',
 	},
 	{
 		name: 'mushroom',
-		img: '/_app/assets/sprites/mushroom.png',
+		image: '/_app/assets/sprites/mushroom.png',
 	},
 	{
 		name: 'sun',
-		img: '/_app/assets/sprites/sun-pixel.png',
+		image: '/_app/assets/sprites/sun-pixel.png',
 	},
 	{
 		name: 'puppy',
-		img: '/_app/assets/sprites/puppy.png',
+		image: '/_app/assets/sprites/puppy.png',
 	},
 	{
 		name: 'turtle',
-		img: '/_app/assets/sprites/turtle.png',
+		image: '/_app/assets/sprites/turtle.png',
 	},
 	{
 		name: 'cat',
-		img: '/_app/assets/sprites/cat.png',
+		image: '/_app/assets/sprites/cat.png',
 	},
 	{
 		name: 'ghost',
-		img: '/_app/assets/sprites/ghost.png',
+		image: '/_app/assets/sprites/ghost.png',
 	},
 	{
 		name: 'mushroom',
-		img: '/_app/assets/sprites/mushroom.png',
+		image: '/_app/assets/sprites/mushroom.png',
 	},
 	{
 		name: 'sun',
-		img: '/_app/assets/sprites/sun-pixel.png',
+		image: '/_app/assets/sprites/sun-pixel.png',
 	},
 	{
 		name: 'puppy',
-		img: '/_app/assets/sprites/puppy.png',
+		image: '/_app/assets/sprites/puppy.png',
 	}
 ]
+
+// console.log(cardArray)
