0a477a111d8415e5459f0a616e220e3a5c7c9178
Anna Eikrem <eikrem.anna@gmail.com>
2023-03-22 19:41:26 +0100

    Created dataArray with objects

---
+++ _app/js/modules/cardData.js
@@ -0,0 +1,50 @@
+export const cardArray = [
+	{
+		name: 'turtle',
+		img: '/_app/assets/sprites/turtle.png',
+	},
+	{
+		name: 'cat',
+		img: '/_app/assets/sprites/cat.png',
+	},
+	{
+		name: 'ghost',
+		img: '/_app/assets/sprites/ghost.png',
+	},
+	{
+		name: 'mushroom',
+		img: '/_app/assets/sprites/mushroom.png',
+	},
+	{
+		name: 'sun',
+		img: '/_app/assets/sprites/sun-pixel.png',
+	},
+	{
+		name: 'puppy',
+		img: '/_app/assets/sprites/puppy.png',
+	},
+	{
+		name: 'turtle',
+		img: '/_app/assets/sprites/turtle.png',
+	},
+	{
+		name: 'cat',
+		img: '/_app/assets/sprites/cat.png',
+	},
+	{
+		name: 'ghost',
+		img: '/_app/assets/sprites/ghost.png',
+	},
+	{
+		name: 'mushroom',
+		img: '/_app/assets/sprites/mushroom.png',
+	},
+	{
+		name: 'sun',
+		img: '/_app/assets/sprites/sun-pixel.png',
+	},
+	{
+		name: 'puppy',
+		img: '/_app/assets/sprites/puppy.png',
+	}
+]
